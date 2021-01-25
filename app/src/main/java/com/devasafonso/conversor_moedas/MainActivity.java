270153fc9d90f7cadc4a6ffe601732e127e3997d
Alexandre Afonso <afonso.s.alexandre@gmail.com>
2021-01-25 17:43:15 -0300

    Initial Commit

---
+++ app/src/main/java/com/devasafonso/conversor_moedas/MainActivity.java
@@ -0,0 +1,58 @@
+package com.devasafonso.conversor_moedas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.mViewHolder.editValue = findViewById(R.id.edit_value);
+        this.mViewHolder.TextDolar = findViewById(R.id.text_dolar);
+        this.mViewHolder.TextEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.ButtonCalculate = findViewById(R.id.button_calculate);
+        this.mViewHolder.ButtonCalculate.setOnClickListener(this);
+        this.clearValues();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button_calculate) {
+            String value = this.mViewHolder.editValue.getText().toString();
+            if ("".equals(value)) {
+                this.clearValues();
+                Toast.makeText(this, this.getString(R.string.informe_valor), Toast.LENGTH_LONG).show();
+            } else {
+                Double valueDouble = Double.valueOf(value);
+                this.mViewHolder.TextDolar.setText(String.format("%.2f", (valueDouble / 4)));
+                this.mViewHolder.TextEuro.setText(String.format("%.2f", (valueDouble / 5)));
+
+            }
+
+        }
+
+    }
+
+    private void clearValues() {
+        this.mViewHolder.TextDolar.setText("");
+        this.mViewHolder.TextEuro.setText("");
+    }
+
+    private static class ViewHolder {
+        EditText editValue;
+        TextView TextDolar;
+        TextView TextEuro;
+        Button ButtonCalculate;
+
+    }
+}
